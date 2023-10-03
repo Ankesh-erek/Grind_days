1b2d5a418b048f1735374b12f5e04a0ff3b117e2
Ankesh-erek <137074724+Ankesh-erek@users.noreply.github.com>
2023-10-03 23:11:33 +0530

    Add files via upload

---
+++ NthFabonacciNumber.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class NthFabonacciNumber {
+    public static void main(String Args[]){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter ");
+
+
+    }
+}
