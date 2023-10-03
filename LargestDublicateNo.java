c70a7e228460962afd538aa6922c472020636d26
Ankesh-erek <137074724+Ankesh-erek@users.noreply.github.com>
2023-10-03 23:11:07 +0530

    Add files via upload

---
+++ LargestDublicateNo.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+public class LargestDublicateNo {
+    public static void main (String[] args){
+        int arr[] = { 2,4,7,2,9,4,3,2};
+        Arrays.sort(arr);
+        
+        for (int i=0; i<7 ; i++){
+            System.out.println(arr[i]);
+            
+                
+                
+
+            
+        }
+
+    }
+}
